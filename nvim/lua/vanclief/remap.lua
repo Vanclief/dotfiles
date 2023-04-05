b07e8940281dfcc51ad76cc2ded2f1a84f058f1a
Franco Valencia <franco.avalencia@gmail.com>
2023-04-05 09:18:27 -0600

    Updated remaps

--- nvim/lua/vanclief/remap.lua
+++ nvim/lua/vanclief/remap.lua
@@ -9,6 +9,12 @@ vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 -- Escape with jj 
 vim.keymap.set("i", "jj", "<Esc>")
 
+-- Easier navigation tmux style
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
 ---------------
 -- Primeagen --
 ---------------
