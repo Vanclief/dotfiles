5804fefcdbdb6fe1e83b94f07498defe10fc49fb
Franco Valencia <franco.avalencia@gmail.com>
2023-04-07 12:38:19 -0600

    Updated harpoon keybindings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>j", ui.nav_next)
-vim.keymap.set("n", "<leader>k", ui.nav_prev)
-vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-f>m", mark.add_file)
+vim.keymap.set("n", "<C-f>n", ui.nav_next)
+vim.keymap.set("n", "<C-f>p", ui.nav_prev)
+vim.keymap.set("n", "<C-f>o", ui.toggle_quick_menu)
