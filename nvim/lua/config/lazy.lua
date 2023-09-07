fa2cb369901e12e3722e064267b9e079653217b7
Franco Valencia <franco.avalencia@gmail.com>
2023-09-07 11:29:08 -0600

    Added prettier

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
