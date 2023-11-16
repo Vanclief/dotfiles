355e33cf593b8b9cc1f3e181d6b4ad210a87cad5
Franco Valencia <franco.avalencia@gmail.com>
2023-11-16 11:01:12 -0600

    Updated Lazyvim & Plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.ui.alpha" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -204,7 +204,7 @@ return {
       gopls = function(_, opts)
         -- workaround for gopls not supporting semanticTokensProvider
         -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
-        require("lazyvim.util").on_attach(function(client, _)
+        require("lazyvim.util").lsp.on_attach(function(client, _)
           if client.name == "gopls" then
             if not client.server_capabilities.semanticTokensProvider then
               local semantic = client.config.capabilities.textDocument.semanticTokens
@@ -231,7 +231,7 @@ return {
         return true
       end,
       ruff_lsp = function()
-        require("lazyvim.util").on_attach(function(client, _)
+        require("lazyvim.util").lsp.on_attach(function(client, _)
           if client.name == "ruff_lsp" then
             -- Disable hover in favor of Pyright
             client.server_capabilities.hoverProvider = false
